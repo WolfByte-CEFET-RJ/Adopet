fdf43628deb121520c565e9e5bac74f6a9ec5273
Natan Balthazar <balthazarnatan@gmail.com>
2020-12-13 23:52:57 -0300

    Tela Chat 1

--- Front-End/src/pages/Inside/Chat/styles.js
+++ Front-End/src/pages/Inside/Chat/styles.js
@@ -1,5 +1,6 @@
 import styled from 'styled-components/native';
 import Constants from 'expo-constants';
+import { BottomTabBar } from '@react-navigation/bottom-tabs';
 
 export const Background = styled.ImageBackground`
   flex: 1;
@@ -13,26 +14,50 @@ export const Container = styled.View`
 
 export const Header = styled.View`
   width: 100%;
-  padding: 10px;
   margin-bottom: 10px;
+  align-items: center;
 `;
 
 export const Title = styled.Image`
 `;
 
-export const Adopt = styled.Text`
-`;
-export const Donate = styled.Text`
+export const HeaderTitle = styled.View`
+  flex-direction: row;
 `;
 
-export const HeaderTitle = styled.View`
+export const Message = styled.Text`
+  font-size: 20px;
+  font-weight: bold;
+`;
+
+export const BottomDonate = styled.TouchableOpacity`
+  background-color:#C4D0CF;
+  align-items: center;
+  justify-content: center;
+  width: 50%;
+  height: 80px;
+  margin: 5px 0px;
+`;
+
+export const BottomAdopt = styled(BottomDonate)`
+  background-color:#E4EEED;
+`;
+
+export const Adopt = styled.Text`
+  font-size: 20px;
+  font-weight: bold;
+`;
+
+export const ListView = styled.View`
+  flex-direction: row;
+  margin: 0px 5px;
   align-items: center;
 `;
 
+export const Donate = styled(Adopt)`
+`;
+
 export const PetArea = styled.View`
-  margin: 5px;
-  position: relative;
-  top: ${props => `${!(props.index % 2) ? 30 : 0 }px`};
 `;
 
 export const Body = styled.FlatList`
